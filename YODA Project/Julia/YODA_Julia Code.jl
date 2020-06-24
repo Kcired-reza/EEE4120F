21a011d5495bf027bb188e6e30e27d4e4f0d3516
Kai Brown <53183488+kaibrown64@users.noreply.github.com>
2020-06-24 18:12:20 +0200

    Added an image

---
+++ YODA Project/Julia/YODA_Julia Code.jl
@@ -0,0 +1,8 @@
+#EEE4120F YODA Project - Image Steganography Decoder
+#Derick Nyende | Tatenda Shuro | Kai Brown
+#NYNDER001 | SHRTAT001 | BRWKAI001
+#Created: 24/06/2020
+#Program to load a .jpg image from memory, decode a secret message into the lsb
+#of the data, and save the encoded image to the disc.
+
+println("Hello World");
