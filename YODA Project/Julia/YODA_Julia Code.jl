91e61500c6e5f747ca4de01086148d089d150d6f
Kai Brown <53183488+kaibrown64@users.noreply.github.com>
2020-06-25 13:49:13 +0200

    Added encoded image
    Successfully able to encode the message, last thing to add is converting the decoded bitvector to a string

--- YODA Project/Julia/YODA_Julia Code.jl
+++ YODA Project/Julia/YODA_Julia Code.jl
@@ -17,30 +17,36 @@ using Images, TestImages, Colors, Plots
 #--------------------------------Create Message:--------------------------
 message = "The turkey has left the oven, I repeat, THE TURKEY HAS LEFT THE OVEN!"
 message = Vector{UInt8}(message) #array of ascii values
-mlen = size(message)[1]*8
+msgE = BitVector()
+for z in message
+    append!(msgE, reverse([z & (0x1<<n) != 0 for n in 0:7]))
+end
+mlen = size(msgE)[1]
 
 #-------------------------Retrieve Image Data:---------------------------
-img = load("YODA_image_original.jpg")
-img = rawview(channelview(Gray.(img)))
-n, m = size(img);
-println(summary(img))
-println("m: ", m, " n: ", n)
+imgO = load("YODA_image_original.jpg")
+imgE = rawview(channelview(Gray.(imgO)))
+save("YODA_image_original_grey.jpg", imgE)
+n, m = size(imgE);
 
 #------------------------Encode Message:---------------------------------
 function encode()
     i = 1   #counter for encoding progress
     for h in 1:n
-        for w in 1:m
-            if i <= mlen
-                nbyte = UInt8(ceil(i/8))
-                if i%8 == 0
-                    nbit = 8
+        for w in 1:m    #loop through pixels
+            lsb = UInt8(imgE[h, w]%2);  #lsb of each pixel
+            if i <= mlen                #still encoding
+                if msgE[i]==1 & lsb==0
+                    imgE[h, w] += 1;
+                elseif msgE[i]==1 & lsb==0
+                    imgE[h, w] -= 1;
                 else
-                    nbit = UInt8(i%8)
+                    continue            #pixel lsb and msgE[1] equal
                 end
-                img[h, w][8] = message[nbyte][nbit]
-            else
-                img[h, w][8] = 0
+            else                        #Past msg length, clear lsb
+                if lsb==1
+                    imgE[h, w] -= 1;
+                end
             end
             i += 1
         end
@@ -50,6 +56,24 @@ end
 encode()
 
 #-----------------------Save Encoded Image:----------------------------
-save("YODA_image_encoded.jpg", img)
+save("YODA_image_encoded.jpg", imgE)
 
 #----------------------Decode Message:---------------------------------
+msgD = BitVector()
+message2 = Vector{UInt8}()
+
+function decode()
+    for h in 1:n
+        for w in 1:m    #loop through pixels
+            lsb = UInt8(imgE[h, w]%2);  #lsb of each pixel
+            append!(msgD, lsb)          #add lsb to decoded message
+        end
+    end
+    for i in 1:(mlen/8)
+        println(i)
+    end
+end
+
+
+
+decode()
